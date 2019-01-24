606fc0ef039d9fed12aed9bd3b87efa11fab60a5
Thiago-Cardoso <trovador182@gmail.com>
2019-01-24 00:20:29 -0200

    creation of component postlist

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { HeaderComponent } from './header/header';
+import { PostListComponent } from './post-list/post-list';
 @NgModule({
-	declarations: [HeaderComponent],
+	declarations: [HeaderComponent,
+    PostListComponent],
 	imports: [IonicModule],
-	exports: [HeaderComponent]
+	exports: [HeaderComponent,
+    PostListComponent]
 })
 export class ComponentsModule {}
